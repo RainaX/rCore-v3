1dc09125347abbe83f24947d3404f66384d31bad
RainaX <xiaofanx@andrew.cmu.edu>
2021-03-09 03:08:44 -0500

    lab2 base code

--- os/src/main.rs
+++ os/src/main.rs
@@ -5,17 +5,18 @@
 #![feature(panic_info_message)]
 
 
-use log::{error, warn, info, debug, trace};
-
-
 #[macro_use]
 mod console;
+mod batch;
 mod lang_items;
 mod logger;
 mod sbi;
+mod syscall;
+mod trap;
 
 
 global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
 
 fn clear_bss() {
     extern "C" {
@@ -28,37 +29,14 @@ fn clear_bss() {
 
 #[no_mangle]
 pub fn rust_main() -> ! {
-    extern "C" {
-        fn stext();
-        fn etext();
-        fn srodata();
-        fn erodata();
-        fn sdata();
-        fn edata();
-        fn sbss();
-        fn ebss();
-        fn boot_stack();
-        fn boot_stack_top();
-    }
     clear_bss();
 
+    println!("[kernel] Hello, world!");
+
     logger::init();
-
-    error!("Hello, world!");
-    warn!("Hello, world!");
-    info!("Hello, world!");
-    debug!("Hello, world!");
-    trace!("Hello, world!");
-
-    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
-        "boot_stack [{:#x}, {:#x})",
-        boot_stack as usize, boot_stack_top as usize
-    );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-    panic!("Shutdown machine!");
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
 
 
