9a79d2b4415d2a2c39890322850cf5d32e140e9d
RainaX <xiaofanx@andrew.cmu.edu>
2021-03-13 17:25:33 -0500

    ch4 sys_get_time implemented

--- os/src/timer.rs
+++ os/src/timer.rs
@@ -3,19 +3,32 @@ use crate::sbi::set_timer;
 use crate::config::CLOCK_FREQ;
 
 const TICKS_PER_SEC: usize = 100;
-const MSEC_PER_SEC: usize = 1000;
+//const MSEC_PER_SEC: usize = 1000;
+const USEC_PER_SEC: usize = 1000000;
 
 pub fn get_time() -> usize {
     time::read()
 }
 
-
+/*
 pub fn get_time_ms() -> usize {
     time::read() / (CLOCK_FREQ / MSEC_PER_SEC)
-}
+}*/
 
 
 pub fn set_next_trigger() {
     set_timer(get_time() + CLOCK_FREQ / TICKS_PER_SEC);
 }
+
+pub fn get_time_val() -> TimeVal {
+    let mut clock = time::read();
+    let sec = clock / CLOCK_FREQ;
+    clock %= CLOCK_FREQ;
+    let usec = clock / (CLOCK_FREQ / USEC_PER_SEC);
+    TimeVal(sec, usec)
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct TimeVal(usize, usize);
 
